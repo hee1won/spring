165d48c3e371e71a1175c47913834ca12ca6db1a
hee1won <happen23@naver.com>
2022-05-09 16:25:56 +0900

    20220509spring

---
+++ src/main/java/org/zerock/controller/ex04/Ex07Controller.java
@@ -0,0 +1,35 @@
+package org.zerock.controller.ex04;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("ex07")
+public class Ex07Controller {
+	
+	@RequestMapping("sub01")
+	public String method01() {
+		// 1. request 파라미터 수집 / 가공 
+		// 2. bussiness 로직 처리 
+		// 3. adds attribute 
+		// 4. forward/redirect
+		
+		return "redirect:https://www.naver.com";
+	}
+	
+	@RequestMapping("sub03")
+	public String method02() {
+		// /ex06/sub01로 redirection
+		
+		return "redirect:/ex06/sub01";
+	}
+	
+	@RequestMapping("sub04")
+	public String method03() {
+		// /ex06/sub05로 리디렉션하는 코드 작성 
+		
+		return "redirect:/ex06/sub05";
+	}
+	
+
+}
